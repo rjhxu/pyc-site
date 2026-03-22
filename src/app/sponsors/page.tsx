7d175a526e1f9aa6bbfe641099168575d03b8919
Ryan <ryanjiahuaxu@gmail.com>
2026-03-22 00:38:37 -0400

    Refactor Sponsors Page to Import Sponsors Data from External File

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -1,19 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-
-const sponsors = [
-    { name: 'Arcadia Earth', logo: '/sponsors/Arcadia Earth_Logo.png', url: 'https://www.arcadiaearth.ca/' },
-    { name: 'Art Gallery of Ontario', logo: '/sponsors/Art Gallery of Ontario Logo.png', url: 'https://ago.ca/' },
-    { name: 'Broadcast Entertainment & Fantasia', logo: '/sponsors/Broadcast Entertainment & Fantasia_Logo.PNG', url: 'https://www.broadcastfantasia.com/' },
-    { name: 'Felix and Norton', logo: '/sponsors/Felix and Norton Logo.png', url: 'https://www.fncookies.com/' },
-    { name: 'Hummingbird Investment Holdings', logo: '/sponsors/Hummingbird Investment Holdings_Logo.jpeg', url: 'https://hummingbirdinvestmentholdings.com/' },
-    { name: 'Jukebox Print', logo: '/sponsors/Jukebox Print_Logo.png', url: 'https://www.jukeboxprint.com/' },
-    { name: 'Presotea', logo: '/sponsors/Presotea_Logo.avif', url: 'https://presotea.ca/' },
-    { name: 'Royal Ontario Museum', logo: '/sponsors/Royal Ontario Museum Logo.jpg', url: 'https://www.rom.on.ca/' },
-    { name: 'Starbucks', logo: '/sponsors/Starbucks.PNG', url: 'https://www.starbucks.ca/' },
-    { name: 'York Region Open Mic', logo: '/sponsors/York Region Open Mic.jpg', url: 'https://linktr.ee/yorkregionpenmic' },
-    { name: 'barBURRITO', logo: '/sponsors/barBURRITO_Logo.png', url: 'https://www.barburrito.ca/' },
-];
+import { sponsors } from '@/data/sponsors';
 
 export default function SponsorsPage() {
     return (
